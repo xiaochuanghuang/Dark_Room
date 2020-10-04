c2c33d2c0db01a5fba48098bb5fe9e4280b663f3
Nate Strohmyer <nate.stroh@live.com>
2020-10-03 19:54:09 -0600

    added a gyro script from YT video, hopefully succesfully got screen touching

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    //public GameObject particle;
+    void Update() {
+        for (int i = 0; i < Input.touchCount; ++i) {
+            if (Input.GetTouch(i).phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                RaycastHit hitRay;
+                if (Physics.Raycast(ray, out hitRay)) {
+                    if (hitRay.collider != null) {
+                        hitRay.collider.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 1, 1);
+                    }
+                    //Instantiate(particle, transform.position, transform.rotation);
+                }
+            }
+        }
+    }
+}

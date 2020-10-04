c95348e4fe24993697a96e196f71f44c36f67465
xiaochuanghuang <35542106+xiaochuanghuang@users.noreply.github.com>
2020-10-04 17:57:17 -0600

    add a test catch function

---
+++ Assets/Scripts/Catch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+    public Transform theDest;
+    private void OnMouseDown()
+    {
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Collider>().enabled = true;
+    }
+}

751914197b7c4e77b920111ecb901b2a1e31c34e
Nate Strohmyer <nate.stroh@live.com>
2020-10-06 00:28:02 -0600

    light should be working and basic interactions when the player lets go of things is implemented

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Catch : MonoBehaviour
 {
     public Transform theDest;
+    public TouchInput TI;
+
     private void OnMouseDown()
     {
-        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<Collider>().enabled = false;
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDest.position;
         this.transform.parent = GameObject.Find("Destination").transform;
+        if (this.gameObject.layer == 9) {
+            TI.hasPhoto = true;
+            TI.hadPhoto = true;
+        }
     }
     private void OnMouseUp()
     {
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<SphereCollider>().enabled = true;
+        GetComponent<Collider>().enabled = true;
+        if (this.gameObject.layer == 9) {
+            TI.hasPhoto = false;
+        }
     }
 }
--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -1,20 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
+
+    public Color startColor;
+    public Color darkRoomColor;
+    public Light _light;
+
+    public bool hasPhoto = false;
+    public bool hadPhoto = false;
+
+    private void Start() {
+        _light.color = startColor;     
+    }
+
+    
+
     //public GameObject particle;
     void Update() {
         for (int i = 0; i < Input.touchCount; ++i) {
+            //begining of each touch
             if (Input.GetTouch(i).phase == TouchPhase.Began) {
                 Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
                 RaycastHit hitRay;
                 if (Physics.Raycast(ray, out hitRay)) {
+                    
                     if (hitRay.collider != null) {
-                        hitRay.collider.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 1, 1);
+                        //hitRay.collider.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 1, 1);
+                        if (hitRay.transform.gameObject.layer == 8) {
+                            Debug.Log("Player tapped light switch");
+                            if (_light.color == startColor)
+                            {
+                                _light.color = darkRoomColor;
+                            }
+                            else {
+                                _light.color = startColor;
+                            }
+                        }
                     }
                     //Instantiate(particle, transform.position, transform.rotation);
+                }
+            }
+
+            //ending a touch
+            if (Input.GetTouch(i).phase == TouchPhase.Ended)
+            {
+                if (!hasPhoto && hadPhoto) {
+                    hadPhoto = false;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    RaycastHit hitRay;
+                    if (Physics.Raycast(ray, out hitRay))
+                    {
+                        if (hitRay.collider != null)
+                        {
+                            //hitRay.collider.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 1, 1);
+                            //player has photo and released on enlarger
+                            if (hitRay.transform.gameObject.layer == 10) {
+                                hitRay.transform.gameObject.GetComponent<Renderer>().material.color = new Color(0, 1, 1, 1);
+                            }
+                        }
+                        //Instantiate(particle, transform.position, transform.rotation);
+                    }
                 }
             }
         }

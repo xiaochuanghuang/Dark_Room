dde28feb3926796d422602070c777d7602e9782d
xiaochuanghuang <35542106+xiaochuanghuang@users.noreply.github.com>
2020-10-06 01:30:38 -0600

    new Interaction

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -4,27 +4,85 @@ using UnityEngine;
 
 public class Catch : MonoBehaviour
 {
-    public Transform theDest;
+    //public Transform theDest;
     public TouchInput TI;
 
-    private void OnMouseDown()
+    //private void OnMouseDown()
+    //{
+    //    GetComponent<Collider>().enabled = false;
+    //    GetComponent<Rigidbody>().useGravity = false;
+    //    this.transform.position = theDest.position;
+    //    this.transform.parent = GameObject.Find("Destination").transform;
+    //    if (this.gameObject.layer == 9)
+    //    {
+    //        TI.hasPhoto = true;
+    //        TI.hadPhoto = true;
+    //    }
+    //}
+    //private void OnMouseUp()
+    //{
+    //    this.transform.parent = null;
+    //    GetComponent<Rigidbody>().useGravity = true;
+    //    GetComponent<Collider>().enabled = true;
+    //    if (this.gameObject.layer == 9)
+    //    {
+    //        TI.hasPhoto = false;
+    //    }
+    //}
+    private Vector3 mOffset;
+
+
+
+    private float mZCoord;
+
+
+
+    void OnMouseDown()
+
     {
-        GetComponent<Collider>().enabled = false;
-        GetComponent<Rigidbody>().useGravity = false;
-        this.transform.position = theDest.position;
-        this.transform.parent = GameObject.Find("Destination").transform;
-        if (this.gameObject.layer == 9) {
-            TI.hasPhoto = true;
-            TI.hadPhoto = true;
-        }
+
+        mZCoord = Camera.main.WorldToScreenPoint(
+
+            gameObject.transform.position).z;
+
+
+        mOffset = gameObject.transform.position - GetFingerAsWorldPoint();
+
     }
-    private void OnMouseUp()
+
+
+
+    private Vector3 GetFingerAsWorldPoint()
+
     {
-        this.transform.parent = null;
-        GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<Collider>().enabled = true;
-        if (this.gameObject.layer == 9) {
-            TI.hasPhoto = false;
-        }
+
+        // Pixel coordinates of mouse (x,y)
+
+        Vector3 mousePoint = Input.mousePosition;
+
+
+
+        // z coordinate of game object on screen
+
+        mousePoint.z = mZCoord;
+
+
+
+        // Convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+
     }
+
+
+
+    void OnMouseDrag()
+
+    {
+
+        transform.position = GetFingerAsWorldPoint() + mOffset;
+
+    }
+
+
 }

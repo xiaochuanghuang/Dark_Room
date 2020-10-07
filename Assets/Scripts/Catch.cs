637b96f2b9bede99f6859558b15d4a9e3ad565f1
Nate Strohmyer <nate.stroh@live.com>
2020-10-06 22:05:01 -0600

    added colliders to chemical baths

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -4,34 +4,8 @@ using UnityEngine;
 
 public class Catch : MonoBehaviour
 {
-    //public Transform theDest;
     public TouchInput TI;
-
-    //private void OnMouseDown()
-    //{
-    //    GetComponent<Collider>().enabled = false;
-    //    GetComponent<Rigidbody>().useGravity = false;
-    //    this.transform.position = theDest.position;
-    //    this.transform.parent = GameObject.Find("Destination").transform;
-    //    if (this.gameObject.layer == 9)
-    //    {
-    //        TI.hasPhoto = true;
-    //        TI.hadPhoto = true;
-    //    }
-    //}
-    //private void OnMouseUp()
-    //{
-    //    this.transform.parent = null;
-    //    GetComponent<Rigidbody>().useGravity = true;
-    //    GetComponent<Collider>().enabled = true;
-    //    if (this.gameObject.layer == 9)
-    //    {
-    //        TI.hasPhoto = false;
-    //    }
-    //}
-
     private Vector3 mOffset;
-
     private float mZCoord;
 
     void OnMouseDown() {

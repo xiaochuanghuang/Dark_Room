8e807f64ca0bc87b73a54409dab1661527b82fd2
Nate Strohmyer <nate.stroh@live.com>
2020-10-06 21:23:49 -0600

    added some interaction functionality

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -29,59 +29,43 @@ public class Catch : MonoBehaviour
     //        TI.hasPhoto = false;
     //    }
     //}
+
     private Vector3 mOffset;
-
-
 
     private float mZCoord;
 
-
-
-    void OnMouseDown()
-
-    {
-
+    void OnMouseDown() {
         mZCoord = Camera.main.WorldToScreenPoint(
-
-            gameObject.transform.position).z;
-
+        gameObject.transform.position).z;
 
         mOffset = gameObject.transform.position - GetFingerAsWorldPoint();
-
+        if (this.gameObject.layer == 9) {
+            TI.hasPhoto = true;
+            TI.hadPhoto = true;
+        }
     }
 
-
-
-    private Vector3 GetFingerAsWorldPoint()
-
-    {
-
+    private Vector3 GetFingerAsWorldPoint() {
         // Pixel coordinates of mouse (x,y)
-
         Vector3 mousePoint = Input.mousePosition;
 
-
-
         // z coordinate of game object on screen
-
         mousePoint.z = mZCoord;
 
-
-
         // Convert it to world points
-
         return Camera.main.ScreenToWorldPoint(mousePoint);
 
     }
 
-
-
-    void OnMouseDrag()
-
-    {
-
+    void OnMouseDrag() {
         transform.position = GetFingerAsWorldPoint() + mOffset;
 
+    }
+
+    void OnMouseUp() {
+        if (this.gameObject.layer == 9) {
+            TI.hasPhoto = false;
+        }
     }
 
 
